164f81f77471da723f062b34e13d05c388380684
rpacheco1 <rodrigo.pacheco@fazenda.rj.gov.br>
2025-06-04 15:30:42 -0300

    Mudanças necessarias

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -12,7 +12,7 @@ import {
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
+  authDomain:   AUTH_DOMAIN,
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
